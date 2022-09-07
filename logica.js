784db1ba69a8034b16ccbf34ff5c3fa4aee7e52a
Cristianps <cristian@MacBook-Air-de-Cristian.local>
2022-09-07 18:12:27 -0500

    Primer commit

---
+++ logica.js
@@ -0,0 +1,21 @@
+
+//alert("prueba");
+var nom = document.getElementById("nombre");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", prueba);
+
+function prueba(){
+    alert("Holi");
+}
+
+var peso = parseFloat();
+var planeta;
+var gtierra = 9.8;
+var gmercurio = 3.7;
+var gvenus = 8.87;
+var gmarte = 3.711;
+var gjupiter = 24.79;
+var gsaturno = 10.74;
+var gurano = 8.69;
+var gneptuno = 11.15;
+
